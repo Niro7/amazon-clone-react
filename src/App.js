92916a809dd56a00961795ee5db8d67486e1065a
K.Niroshani <niroshini.07.skk@gmail.com>
2020-09-21 23:27:32 +0530

    ADD: React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Checkout from "./Checkout";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        {/* Header component is repeated so it can be declared outside Switch */}
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
