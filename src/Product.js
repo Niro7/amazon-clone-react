5632aa1269fda6683c08e6ba8ca2a08245aba3a0
K.Niroshani <niroshini.07.skk@gmail.com>
2020-09-21 11:30:24 +0530

    ADD Product components, items

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,32 @@
 import React from "react";
 import "./Product.css";
-function Product() {
-  return <div className="product"></div>;
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating) /*Creating an empty Array to store stars */
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button>
+        <span>Add to Basket</span>
+        <span className="product__btn_icon">
+          <ShoppingCartIcon />
+        </span>
+      </button>
+    </div>
+  );
 }
 
 export default Product;
