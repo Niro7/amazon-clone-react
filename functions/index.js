f03a072d9eb164c9613491e39c12e72177655f60
K.Niroshani <niroshini.07.skk@gmail.com>
2020-10-21 03:30:33 +0530

    ADD: functions index.js file

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
+const { request, response } = require('express');
+const stripe = require("stripe")('sk_test_51HPvU8KOB9zeWUAqtZfdvAChu66MSi12NSvBWYTkVfwq7R0lEKdfulGkJf7vVamgwh1MzCAwdIny68FRlv8m04ng00K6NboTEk')
+
+// App config 
+const app = express();
+
+// Middle ware
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get('/',(request, response) => response.status(200).send('hello world'))
+
+app.post('/payments/create', async (request, response) =>{
+    const total = request.query.total;
+    console.log("Payment Request Received!!! for this amount >>>", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd"
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+
+// Listen command
+exports.api = functions.https.onRequest(app)
+
+// Example endpoint
+// http://localhost:5001/clone-challenge-51a15/us-central1/api
