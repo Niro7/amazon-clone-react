ad620499d7fe72a1fd8725220948a7637bd1078d
K.Niroshani <niroshini.07.skk@gmail.com>
2020-10-21 03:32:36 +0530

    UPDATE: baseURL of cloud functions

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-challenge-51a15/us-central1/api'
+    baseURL: 'https://us-central1-clone-challenge-51a15.cloudfunctions.net/api'
+    // 'http://localhost:5001/clone-challenge-51a15/us-central1/api'
+    
     // API the (cloud function) URL
 });
 
